de490f7d5ea1a7f7ba7fa713ec5ca3a6d49b45bc
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-28 03:38:58 +0300

    solved

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function isPalindrome(word) {
+  for (var i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
   // Write your algorithm here
 }
 
